30e2f5c7b041486d2b4b7c25c837ff0a5dddfecb
Felix Yuan <felix.yuan@emc.com>
2016-04-14 17:59:17 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,47 @@
+#!/bin/bash
+
+export PORT=3000
+
 git clone https://github.com/cgx027/fishing.git 2>/dev/null
 cd fishing/
-#git fetch
-changed=0;sudo git pull| grep -q -v 'Already up-to-date.' && changed=1
-#echo changed=$changed
-if [  $changed = 1  ]; then
-    echo "new commit found"
-else
-    echo "no new commit found"
-fi
-npm install
-export PORT=4000; npm start
+
+echo "Start process"
+npm start &
+current_pid=$!
+echo "process started, pid="$current_pid
+
+while :
+do
+    date
+    #git fetch
+    echo "checking for new commits"
+    
+    #sudo git pull| grep -q -v 'Already up-to-date.' && changed=1
+    #changed=0;sudo ls| grep -q -v 'test' && changed=1
+    
+    git fetch
+    LOCAL=$(git log -p -1])
+    #echo LOCAL = $LOCAL
+    REMOTE=$(git log -p -1 origin/master)
+    #echo REMOTE = $REMOTE
+    #BASE=$(git merge-base @ @{u})
+    #echo BASE = $BASE
+    
+    if [ "$LOCAL" = "$REMOTE" ]; then
+        echo "No new commit found"
+    else        
+        echo "New commit found"
+        echo "Fetching new code"
+        git fetch
+        git rebase origin/master
+        echo "Install packages"
+        npm install
+        echo "Stop existing running process"
+        sudo kill $current_pid 2>/dev/null
+    fi
+    
+    echo
+    echo
+    
+    sleep 5
+done
